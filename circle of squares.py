6bf8331c55de6cb96e597179b8268340e383adbf
hemusaisiva <hemusaisiva@gmail.com>
2017-04-03 01:06:06 -0700

    circles angle changed

--- circle of squares.py
+++ circle of squares.py
@@ -1,16 +1,13 @@
 import turtle
 angle=95
 my_turtle=turtle.Turtle()
+my_turtle.speed(0)
 def square(length,angle):
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
+    for i in range(4):
+        my_turtle.forward(length)
+        my_turtle.right(angle)
 
-for i in range(36):
-    square(65,90)
-    my_turtle.right(angle+5)
+for i in range(250):
+    square(100,90)
+    my_turtle.right(11)
     
